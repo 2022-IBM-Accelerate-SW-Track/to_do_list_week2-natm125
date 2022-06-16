8cfe381f49298f7347ad887c573350267ec961a4
Natm492 <mohanty7@illinois.edu>
2022-06-15 22:20:24 -0400

    finished part 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,10 @@ class Home extends Component {
   addTodo = (todo) => {
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
+    //we are going to create an if condition to check whether an item has already been added
+    if (this.state.todos.find(element => element.content === todo.content)) {
+      return;
+    }
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
